995676f4f7a9ff014bcf298a8d55d99b14b42314
Richard Das <richard@richarddas.com>
2015-03-28 20:12:55 +0000

    Added access to all customizable params for UIAppearance

--- MONActivityIndicatorViewDemo/MONActivityIndicatorViewDemo/Source/Views/MONActivityIndicatorView/MONActivityIndicatorView.h
+++ MONActivityIndicatorViewDemo/MONActivityIndicatorViewDemo/Source/Views/MONActivityIndicatorView/MONActivityIndicatorView.h
@@ -12,19 +12,19 @@ IB_DESIGNABLE
 @interface MONActivityIndicatorView : UIView <UIAppearanceContainer>
 
 /** The number of circle indicators. */
-@property (nonatomic) IBInspectable NSUInteger numberOfCircles;
+@property (nonatomic) IBInspectable NSUInteger numberOfCircles; //UI_APPEARANCE_SELECTOR
 
 /** The spacing between circles. */
-@property (nonatomic) IBInspectable CGFloat internalSpacing;
+@property (nonatomic) IBInspectable CGFloat internalSpacing; //UI_APPEARANCE_SELECTOR
 
 /** The radius of each circle. */
-@property (nonatomic) IBInspectable CGFloat radius;
+@property (nonatomic) IBInspectable CGFloat radius; //UI_APPEARANCE_SELECTOR
 
 /** The base animation delay of each circle. */
-@property (nonatomic) IBInspectable CGFloat delay;
+@property (nonatomic) IBInspectable CGFloat delay; //UI_APPEARANCE_SELECTOR
 
 /** The base animation duration of each circle*/
-@property (nonatomic) IBInspectable CGFloat duration;
+@property (nonatomic) IBInspectable CGFloat duration; //UI_APPEARANCE_SELECTOR
 
 /** The assigned delegate */
 @property (nonatomic, weak) id<MONActivityIndicatorViewDelegate> delegate;
